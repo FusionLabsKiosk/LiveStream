17327c62e5f5c165dc65ecdaf700dd6f3591c2b4
FusionLabsKiosk <fusionlabskiosk@gmail.com>
2014-05-28 14:15:35 -0500

    Added Start/End functions to widget JS

--- widgets/demo/demo.js
+++ widgets/demo/demo.js
@@ -4,3 +4,11 @@ var demo = {};
 demo.initialize = function() {
     //Any initialization here
 };
+
+demo.start = function() {
+    //Widget start logic
+};
+
+demo.end = function() {
+    //Widget ending logic
+};
