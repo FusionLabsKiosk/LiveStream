fb0c25aa671545c2382f84bdabbb774e7b3ddf8e
FusionLabsKiosk <fusionlabskiosk@gmail.com>
2014-05-30 09:00:07 -0500

    Cleaned up maps widget code and removed bug where multiple copies of Maps API would be inserted

---
+++ widgets/maps/sandbox.js
@@ -0,0 +1,41 @@
+var map;
+
+var currentLat;
+var currentLng;
+
+function initializeMap() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+            'callback=drawMap';
+    document.body.appendChild(script);
+}
+function drawMap() {
+    map = new google.maps.Map(document.getElementById("map-canvas"), {
+        'center': {
+            'lat': currentLat,
+            'lng': currentLng
+        },
+        'zoom': 8
+    });
+}
+
+function updateMap(e) {
+    if (e.data.widget === 'maps') {
+        currentLat = parseFloat(e.data.lat);
+        currentLng = parseFloat(e.data.lng);
+        if (map === undefined) {
+            initializeMap();
+        }
+        else {
+            map.setCenter({
+                'lat': currentLat,
+                'lng': currentLng
+            });
+        }
+    }
+}
+
+window.onload = function() {
+    window.addEventListener('message', updateMap);
+};
